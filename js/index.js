453f1874c70160742a1c5073caf20542d3be70f3
yukari-tennis <rinako358@gmail.com>
2026-07-20 17:20:20 +0900

    初期画面作成
    Bootstrapによる初期画面を作成
    index.html、config.js、index.jsを追加

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Management Timeline 起動");
+    const searchButton = document.getElementById("search");
+    searchButton.addEventListener("click", () => {
+        alert("検索機能はこれから作ります。");
+    });
+});
