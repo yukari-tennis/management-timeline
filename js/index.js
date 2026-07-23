37e9b372c67036f7afbce9a306d89e91c0d39c27
yukari-tennis <rinako358@gmail.com>
2026-07-24 06:14:23 +0900

    人物プルダウンもDBからとってくるよう変更

--- js/index.js
+++ js/index.js
@@ -22,3 +22,77 @@ async function loadCategories() {
 }
 
 loadCategories();
+
+async function loadPeoples() {
+  const { data, error } = await supabaseClient
+    .from("people")
+    .select("*")
+    .order("id");
+
+  if (error) {
+    console.error("人物取得エラー:", error);
+    return;
+  }
+
+  const categorySelect = document.getElementById("person");
+
+  data.forEach((person) => {
+    const option = document.createElement("option");
+
+    option.value = person.id;
+    option.textContent = person.name;
+
+    categorySelect.appendChild(option);
+  });
+}
+
+loadPeoples();
+
+async function loadEvents() {
+  const { data, error } = await supabaseClient
+    .from("events")
+    .select(`
+      id,
+      event_date_text,
+      title,
+      people (
+        name
+      ),
+      categories (
+        name
+      )
+    `)
+    .order("sort_order", { ascending: true })
+    .order("id", { ascending: true });
+
+  if (error) {
+    console.error("年表取得エラー:", error);
+    return;
+  }
+
+  console.log("events =", data);
+
+  const eventTable = document.getElementById("eventTable");
+  eventTable.innerHTML = "";
+
+  data.forEach((event) => {
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+      <td>${event.event_date_text ?? ""}</td>
+      <td>${event.title ?? ""}</td>
+      <td>${event.people?.name ?? ""}</td>
+      <td>${event.categories?.name ?? ""}</td>
+      <td></td>
+      <td>
+        <button class="btn btn-sm btn-outline-primary">
+          編集
+        </button>
+      </td>
+    `;
+
+    eventTable.appendChild(row);
+  });
+}
+
+loadEvents();
